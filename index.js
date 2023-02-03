70e1b4e39b294acb0b68766172b9af844dfae7fb
RGBrain <31857067+RGBrain@users.noreply.github.com>
2023-02-03 22:27:56 +0000

    Pass all tests

--- index.js
+++ index.js
@@ -13,3 +13,42 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const mngrQuestions = [
+    {
+      type: "input",
+      name: "name",
+      message: "What is the team manager's name?"
+    }, 
+    {
+      type: "input",
+      name: "id",
+      message: "What is the manager's employee id?"
+    },        
+    {
+      type: "input",
+      name: "email",
+      message: "What is the manager's email address?"
+    },        
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is the project name?"
+    },
+    {
+      type: "list",
+      name: "additionalStaff",
+      message: "Would you now like to do next?  Choices: ",
+      choices: ['Add an Engineer', 'Add an intern', "I've finished adding staff"]
+    }
+];
+
+// function to initialize program
+function init() {
+    inquirer.prompt(mngrQuestions)
+    .then(function(response){
+    console.log(`Thank you for entering those details for ${response.name}`);
+    });
+}
+
+// function call to initialize program
+init();
